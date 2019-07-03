2913db4a81a4b1f2e3bfaba7ba5234f1388f9d90
Martin Krisell <martin.krisell@gmail.com>
2019-07-03 16:52:41 +0000

    Apply fixes from StyleCI

--- tests/RedirectWWWTest.php
+++ tests/RedirectWWWTest.php
@@ -4,14 +4,15 @@ namespace Krisell\RedirectWWWMiddleware\Tests;
 
 use Orchestra\Testbench\TestCase;
 use Illuminate\Support\Facades\Route;
-use \Krisell\RedirectWWWMiddleware\Http\Middleware\RedirectWWW;
+use Krisell\RedirectWWWMiddleware\Http\Middleware\RedirectWWW;
 
 class RedirectWWWTest extends TestCase
 {
     /** @test */
     public function it_redirects_the_www_subdomain_to_main_domain()
     {
-        Route::get('route', function () {})->middleware(RedirectWWW::class);
+        Route::get('route', function () {
+        })->middleware(RedirectWWW::class);
 
         $this->get('http://www.localhost/route')->assertRedirect('http://localhost/route');
     }
@@ -19,7 +20,8 @@ class RedirectWWWTest extends TestCase
     /** @test */
     public function it_does_not_redirect_other_subdomains_or_no_subdomain()
     {
-        Route::get('route', function () {})->middleware(RedirectWWW::class);
+        Route::get('route', function () {
+        })->middleware(RedirectWWW::class);
 
         $this->get('http://subdomainwww.localhost/route')->assertStatus(200);
         $this->get('http://subdomain.localhost/route')->assertStatus(200);
@@ -30,7 +32,8 @@ class RedirectWWWTest extends TestCase
     /** @test */
     public function it_does_not_affect_other_routes_than_those_that_uses_the_middleware()
     {
-        Route::get('route', function () {})->middleware();
+        Route::get('route', function () {
+        })->middleware();
 
         $this->get('http://subdomainwww.localhost/route')->assertStatus(200);
         $this->get('http://subdomain.localhost/route')->assertStatus(200);
