2b0730dfa09acd47a587c360fb0493055c7a42db
Martin Krisell <martin.krisell@gmail.com>
2019-07-03 16:34:52 +0200

    Initial commit

---
+++ src/Http/Middleware/RedirectWWW.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Krisell\RedirectWWWMiddleware\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Redirect;
+
+class RedirectWWW
+{
+    /**
+     * Peeks into the incoming request and if the subdomain is
+     * www, it redirects to main domain.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (starts_with($request->header('host'), 'www.')) {
+            $host = str_replace("www.", '', $request->header('host'));
+            $request->headers->set('host', $host);
+
+            return Redirect::to($request->fullUrl(), 301);
+        }
+
+        return $next($request);
+    }
+}
